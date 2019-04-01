301815c362437c658c9d43f30f851b524c166329
Serhii <serhey.f5@gmail.com>
2019-04-01 12:03:47 +0300

    add footer

--- wp-content/themes/arc.test/footer.php
+++ wp-content/themes/arc.test/footer.php
@@ -3,18 +3,24 @@
 		<div class="page-container">
 
 			<div class="footer__logo">
-				<a href=""></a>
+				<a href="#">
+				STRUCTURES
+				</a>
 			</div>
 
-			<div class="footer-address-block">
+			<div class="address-block">
+				<p class="address-block">3900 WESTERN WAY NE,
+				MILLERSBURG, OR 97321
+				</p>
+				<p>TELEPHONE: (541) 367-2929</p>
+				<p>FAX: (541) 367-2950</p>
+			</div>
+
+			<div class="menu-block">
 
 			</div>
 
-			<div class="footer-menu-block">
-
-			</div>
-
-			<div class="footer-subs-block">
+			<div class="subs-block">
 
 			</div>
 
